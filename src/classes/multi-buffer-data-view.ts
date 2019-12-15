8a4ec2b80ad6058fdbb6003db2dd1554da6ef466
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-12-15 19:49:11 +0100

    fix: exclude all BigInt related functions

--- src/classes/multi-buffer-data-view.ts
+++ src/classes/multi-buffer-data-view.ts
@@ -1,4 +1,4 @@
-export class MultiBufferDataView implements Omit<DataView, 'buffer'> {
+export class MultiBufferDataView implements Omit<DataView, 'buffer' | 'getBigInt64' | 'getBigUint64' | 'setBigInt64' | 'setBigUint64'> {
 
     private _buffers: ArrayBuffer[];
 
